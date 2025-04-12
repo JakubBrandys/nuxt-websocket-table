31751adf5ee6f9bfbeaac7526da79720250c6fa0
Jakub Brandys <kuba.brandys99@gmail.com>
2025-04-12 23:14:35 +0200

    NWT-01: create NavigationBar component

---
+++ components/NavigationBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="bg-white shadow">
+    <div class="container mx-auto p-4 flex justify-between items-center">
+      <h1 class="text-2xl font-bold">Zendo Ticket</h1>
+      <ul class="flex gap-6 text-sm font-medium">
+        <li>
+          <NuxtLink to="/" exact-active-class="text-blue-600 font-semibold"> Orders </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script setup lang="ts"></script>
